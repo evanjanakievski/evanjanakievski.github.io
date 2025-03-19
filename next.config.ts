ebca1fb21ada51598405e7e861f45f3dad8cd00f
Evan Janakievski <eejanak24@gmail.com>
2025-03-18 23:14:24 -0400

    config

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,18 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-  output: "export",
-};
-
-export default nextConfig;
+const nextConfig = {
+  output: 'export',
+ 
+  // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
+  // trailingSlash: true,
+ 
+  // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
+  // skipTrailingSlashRedirect: true,
+ 
+  // Optional: Change the output directory `out` -> `dist`
+  // distDir: 'dist',
+}
+ 
+module.exports = {nextConfig, images: {
+  domains: ['evanjanakievski.com'],
+}}
